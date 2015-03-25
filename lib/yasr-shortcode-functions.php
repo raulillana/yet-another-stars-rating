c89f2f7a1af7ded3e760d0fde27a6846cab057bd
Raul Illana <raul.fernandez.illana@gmail.com>
2015-03-25 22:17:30 +0100

    Added shortcode [yasr_visitor_votes_only_stars]
    Added shortcode [yasr_visitor_votes_only_stars] and shortcode_visitor_votes_only_stars_callback()

--- lib/yasr-shortcode-functions.php
+++ lib/yasr-shortcode-functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /*
 
@@ -68,13 +68,13 @@ function shortcode_overall_rating_callback ($atts) {
             $px_size = '32';
         }
 
-        $shortcode_html .= "<div class=\"$rateit_class\" id=\"yasr_rateit_overall\" data-rateit-starwidth=\"$px_size\" data-rateit-starheight=\"$px_size\" data-rateit-value=\"$overall_rating\" data-rateit-step=\"0.1\" data-rateit-resetable=\"false\" data-rateit-readonly=\"true\"></div>"; 
+        $shortcode_html .= "<div class=\"$rateit_class\" id=\"yasr_rateit_overall\" data-rateit-starwidth=\"$px_size\" data-rateit-starheight=\"$px_size\" data-rateit-value=\"$overall_rating\" data-rateit-step=\"0.1\" data-rateit-resetable=\"false\" data-rateit-readonly=\"true\"></div>";
 
 
         if (YASR_TEXT_BEFORE_STARS == 1 && YASR_TEXT_BEFORE_OVERALL != '') {
 
             $shortcode_html .= "</div>";
-        
+
         }
 
         //IF show overall rating in loop is disabled use is_singular && is_main query
@@ -181,7 +181,7 @@ function shortcode_visitor_votes_callback ($atts) {
             //Chek if a logged in user has already rated for this post
             $vote_if_user_already_rated = yasr_check_if_user_already_voted();
 
-            //If user has already rated 
+            //If user has already rated
             if ($vote_if_user_already_rated) {
 
                 $span_after_rate_it="<span class=\"yasr-small-block-bold\" id=\"yasr-already-voted-text\">" . __("You've already voted this article with", "yasr") . " $vote_if_user_already_rated </span>";
@@ -189,7 +189,7 @@ function shortcode_visitor_votes_callback ($atts) {
             }
 
         } //End if user is logged
-  
+
     } //end if  ($allow_logged_option['allowed_user']==='allow_anonymous') {
 
 
@@ -252,7 +252,7 @@ function shortcode_visitor_votes_callback ($atts) {
     if(YASR_TEXT_BEFORE_STARS == 1 && YASR_TEXT_BEFORE_VISITOR_RATING != '') {
 
         $shortcode_html_tmp = "<div class=\"yasr-container-custom-text-and-visitor-rating\">
-        <span id=\"yasr-custom-text-before-visitor-rating\">" . YASR_TEXT_BEFORE_VISITOR_RATING . "</span>" .  $shortcode_html . "</div>"; 
+        <span id=\"yasr-custom-text-before-visitor-rating\">" . YASR_TEXT_BEFORE_VISITOR_RATING . "</span>" .  $shortcode_html . "</div>";
 
         $shortcode_html = $shortcode_html_tmp;
 
@@ -288,14 +288,14 @@ function shortcode_visitor_votes_callback ($atts) {
                 var voteIfUserAlredyRated = $var_vote_if_user_already_rated;
                 var loaderHtml = $var_loader_html;
                 var nonceVisitor = $var_nonce_visitor;
-                    
+
                 yasrVisitorsVotes(arrayTooltipValues, postid, ajaxurl, size, loggedUser, voteIfUserAlredyRated, loaderHtml, nonceVisitor);
 
                 var visitorStatsEnabled = $var_visitor_stats_enabled;
 
-                //If stats are enabled call the function 
+                //If stats are enabled call the function
                 if (visitorStatsEnabled == 'yes') {
-                    yasrDrawTipsProgress (postid, ajaxurl); 
+                    yasrDrawTipsProgress (postid, ajaxurl);
                 }
 
             });
@@ -338,7 +338,7 @@ function shortcode_multi_set_callback( $atts ) {
 	$post_id=get_the_id();
 
 	global $wpdb;
-	
+
 	// Attributes
 	extract( shortcode_atts(
 		array(
@@ -361,8 +361,8 @@ function shortcode_multi_set_callback( $atts ) {
     //If there is not vote for that set...i.e. add shortcode without initialize it
     else {
     	$set_name=$wpdb->get_results("SELECT field_name AS name, field_id AS id
-                    FROM " . YASR_MULTI_SET_FIELDS_TABLE . "  
-                    WHERE parent_set_id=$setid 
+                    FROM " . YASR_MULTI_SET_FIELDS_TABLE . "
+                    WHERE parent_set_id=$setid
                     ORDER BY field_id ASC");
 
     	$shortcode_html="<table class=\"yasr_table_multi_set_shortcode\">";
@@ -373,7 +373,7 @@ function shortcode_multi_set_callback( $atts ) {
         						 </tr>";
         }
     	$shortcode_html.="</table>";
-    	
+
     }
 	return $shortcode_html;
 	} //End function
@@ -440,14 +440,14 @@ function yasr_most_or_highest_rated_posts_callback () {
     global $wpdb;
 
     $query_result_most_rated = $wpdb->get_results("SELECT post_id, number_of_votes, sum_votes
-                                        FROM " . YASR_VOTES_TABLE . ", $wpdb->posts AS p 
+                                        FROM " . YASR_VOTES_TABLE . ", $wpdb->posts AS p
                                         WHERE post_id = p.ID
                                         AND number_of_votes >= 1
                                         AND p.post_status = 'publish'
                                         ORDER BY number_of_votes DESC, sum_votes DESC LIMIT 10");
 
     $query_result_highest = $wpdb->get_results("SELECT (sum_votes / number_of_votes) as result, post_id, number_of_votes
-                                        FROM " . YASR_VOTES_TABLE . ", $wpdb->posts AS p 
+                                        FROM " . YASR_VOTES_TABLE . ", $wpdb->posts AS p
                                         WHERE post_id = p.ID
                                         AND number_of_votes >= 2
                                         AND p.post_status = 'publish'
@@ -491,7 +491,7 @@ function yasr_most_or_highest_rated_posts_callback () {
         $shortcode_html = __("You've not enough data","yasr") . "<br />";
     }
 
-    
+
     if ($query_result_highest) {
 
         $shortcode_html .= "<table class=\"yasr-table-chart\" id=\"yasr-highest-rated-posts\">
@@ -556,7 +556,7 @@ function yasr_top_5_reviewers_callback () {
     global $wpdb;
 
     $query_result = $wpdb->get_results("SELECT COUNT( post_author ) as total_count, post_author as reviewer
-                                        FROM $wpdb->posts AS p, " . YASR_VOTES_TABLE . " 
+                                        FROM $wpdb->posts AS p, " . YASR_VOTES_TABLE . "
                                         WHERE  post_id = p.ID
                                         AND p.post_status = 'publish'
                                         GROUP BY post_author
@@ -589,7 +589,7 @@ function yasr_top_5_reviewers_callback () {
                 $user_profile = '#';
                 $user_data = new stdClass;
                 $user_data->user_login = 'Anonymous';
-            
+
             }
 
 
@@ -597,7 +597,7 @@ function yasr_top_5_reviewers_callback () {
                                     <td><a href=\"$user_profile\">$user_data->user_login</a></td>
                                     <td>$result->total_count</td>
                                 </tr>";
-                                
+
         }
 
         $shortcode_html .= "</table>";
@@ -631,7 +631,7 @@ function yasr_top_ten_active_users_callback () {
                                         FROM " . YASR_LOG_TABLE . ", $wpdb->posts AS p
                                         WHERE  post_id = p.ID
                                         AND p.post_status = 'publish'
-                                        GROUP BY user_id 
+                                        GROUP BY user_id
                                         ORDER BY ( total_count ) DESC
                                         LIMIT 10");
 
@@ -681,5 +681,168 @@ function yasr_top_ten_active_users_callback () {
 
 
 } //End function
+
+/****** Add shortcode for user vote ******/
+
+add_shortcode ('yasr_visitor_votes_only_stars', 'shortcode_visitor_votes_only_stars_callback');
+
+function shortcode_visitor_votes_only_stars_callback ($atts) {
+
+    $shortcode_html = NULL; //Avoid undefined variable outside is_singular && is_main_query
+
+    $post_id = get_the_ID();
+
+    $ajax_nonce_visitor = wp_create_nonce( "yasr_nonce_insert_visitor_rating" );
+
+    $votes=yasr_get_visitor_votes();
+
+    $medium_rating=0;   //Avoid undefined variable
+
+    if (!$votes) {
+        $votes=0;         //Avoid undefined variable if there is not overall rating
+        $votes_number=0;  //Avoid undefined variable
+    }
+
+    else {
+        foreach ($votes as $user_votes) {
+            $votes_number = $user_votes->number_of_votes;
+            if ($votes_number != 0 ) {
+                $medium_rating = ($user_votes->sum_votes/$votes_number);
+            }
+            else {
+                $medium_rating = 0;
+            }
+        }
+    }
+
+    $image = YASR_IMG_DIR . "/loader.gif";
+
+    $loader_html = '<div id="loader-visitor-rating" >&nbsp; ' . __("Loading, please wait","yasr") . ' <img src=' .  "$image" .' title="yasr-loader" alt="yasr-loader"></div>';
+
+    $medium_rating=round($medium_rating, 1);
+
+    if (!$atts) {
+        $size = 'large';
+    }
+
+    else {
+        extract( shortcode_atts (
+            array(
+                'size' => 'string',
+            ), $atts )
+        );
+    }
+
+    if ($size === 'small') {
+        $rateit_class='rateit';
+        $px_size = '16';
+    }
+
+    elseif ($size === 'medium') {
+        $rateit_class = 'rateit medium';
+        $px_size = '24';
+    }
+
+    //default values
+    else {
+        $rateit_class = 'rateit bigstars';
+        $px_size = '32';
+    }
+
+    $vote_if_user_already_rated = FALSE;
+
+    $shortcode_html = "<div id=\"yasr_visitor_votes_$post_id\" class=\"yasr-visitor-votes\">";
+    $span_after_rate_it = "";
+
+    //if anonymous are allowed to vote
+    if (YASR_ALLOWED_USER === 'allow_anonymous') {
+
+        $readonly = 'false'; //readonly is ALWAYS false
+
+        //I've to block a logged in user that has already rated
+        if ( is_user_logged_in() ) {
+
+            //Chek if a logged in user has already rated for this post
+            $vote_if_user_already_rated = yasr_check_if_user_already_voted();
+
+            //If user has already rated
+            if ($vote_if_user_already_rated) {
+
+                $span_after_rate_it="<span class=\"yasr-small-block-bold\" id=\"yasr-already-voted-text\">" . __("You already voted this article with", "yasr") . " $vote_if_user_already_rated </span>";
+
+            }
+
+        } //End if user is logged
+
+    } //end if  ($allow_logged_option['allowed_user']==='allow_anonymous') {
+
+
+    //If only logged in users can vote
+    elseif (YASR_ALLOWED_USER === 'logged_only') {
+
+        //If user is logged in and can vote
+        if ( is_user_logged_in() ) {
+
+            $readonly = 'false'; //REadonly is false if user is logged
+
+            //Chek if a logged in user has already rated for this post
+            $vote_if_user_already_rated = yasr_check_if_user_already_voted();
+
+            if ($vote_if_user_already_rated) {
+
+                $span_after_rate_it="<span class=\"yasr-small-block-bold\" id=\"yasr-already-voted-text\">" . __("You already voted this article with", "yasr") . " $vote_if_user_already_rated </span>";
+
+            }
+
+        } //End if user is logged in
+
+        //Else mean user is not logged in and can't vote
+        else {
+
+            $readonly = 'true'; //readonly is true if user isn't logged
+
+            $span_after_rate_it = __("You must sign to vote", "yasr");
+
+        }
+
+    }
+
+    $shortcode_html .= "<div class=\"$rateit_class\" id=\"yasr_rateit_visitor_votes_$post_id\" data-rateit-starwidth=\"$px_size\" data-rateit-starheight=\"$px_size\" data-rateit-value=\"$medium_rating\" data-rateit-step=\"1\" data-rateit-resetable=\"false\" data-rateit-readonly=\"true\"></div>";
+
+    $shortcode_html .= "</div>";
+
+
+
+    if(YASR_TEXT_BEFORE_STARS == 1 && YASR_TEXT_BEFORE_VISITOR_RATING != '') {
+
+        $shortcode_html_tmp = "<div class=\"yasr-container-custom-text-and-visitor-rating\">
+        <span id=\"yasr-custom-text-before-visitor-rating\">" . YASR_TEXT_BEFORE_VISITOR_RATING . "</span>" .  $shortcode_html . "</div>";
+
+        $shortcode_html = $shortcode_html_tmp;
+
+    }
+
+
+        //IF show visitor votes in loop is disabled use is_singular && is_main query
+        if ( YASR_SHOW_VISITOR_VOTES_IN_LOOP === 'disabled' ) {
+
+            if( is_singular() && is_main_query() ) {
+
+                return $shortcode_html;
+
+            }
+
+        } // End if YASR_SHOW_VISITOR_VOTES_IN_LOOP === 'disabled') {
+
+        //If overall rating in loop is enabled don't use is_singular && is main_query
+        elseif ( YASR_SHOW_VISITOR_VOTES_IN_LOOP === 'enabled' ) {
+
+            return $shortcode_html;
+
+        }
+
+   // } //End (!is_feed)
+
+} //End function shortcode_visitor_votes_only_stars_callback
 
 ?>
